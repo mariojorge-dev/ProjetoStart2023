c1f161a9c4adea560fd334b639eb8539f740d90e
mariojorge-dev <108435653+mariojorge-dev@users.noreply.github.com>
2023-10-28 22:43:59 -0300

    Ajustes
    Ajuste no javascript

--- script.js
+++ script.js
@@ -5,51 +5,43 @@ const Inome = document.querySelector(".nome");
 const Icpf = document.querySelector(".cpf");
 const Icontato = document.querySelector(".contato");
 const Idatanasc = document.querySelector(".datanasc");
-const Isexo = document.querySelector(".sexo");
+const Isexo = document.querySelector(".sexo");/*
 const Icep = document.querySelector(".cep");
 const Icidade = document.querySelector(".cidade");
 const Iestado = document.querySelector(".estado");
-const Iendereco = document.querySelector(".endereco");
+const Iendereco = document.querySelector(".endereco");*/
 const Isenha = document.querySelector(".senha");
 
-
 function cadastrar() {
-
-    fetch("http://localhost:8080/cadastrar",
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type:': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify({
-                nome: Inome.value,
-                cpf: Icpf.value,
-                contato: Icontato.value,
-                idade: Idatanasc.value,
-                sexo: Isexo.value,
-                cep: Icep.value,
-                cidade: Icidade.value,
-                estado: Iestado.value,
-                endereco: Iendereco.value,
-                senha: Isenha.value
-            })
+    fetch("http://localhost:8080/usuarios", {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json' // Correção aqui
+        },
+        method: "POST",
+        body: JSON.stringify({
+            nome: Inome.value,
+            cpf: Icpf.value,
+            contato: Icontato.value,
+            idade: Idatanasc.value,
+            sexo: Isexo.value,
+            senha: Isenha.value
         })
-        .then(function (res) { console.log(res) })
-        .catch(function (res) { console.log(res) })
-
-};
+    })
+    .then(function (res) { console.log(res) })
+    .catch(function (res) { console.log(res) })
+}
 
 function limpar () {
     Inome.value = "";
     Icpf.value = "";
     Icontato.value = "";
     Idatanasc.value = "";
-    Isexo.value = "";
+    Isexo.value = "";/*
     Icep.value = "";
     Icidade.value = "";
     Iestado.value = "";
-    Iendereco.value = "";
+    Iendereco.value = "";*/
     Isenha.value = "";
     
 };
