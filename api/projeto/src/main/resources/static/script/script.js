c51c8bb01b74da979ec6a845f17a47fcd33399b0
mariojorge-dev <108435653+mariojorge-dev@users.noreply.github.com>
2023-12-04 15:50:19 -0300

    Validando campo Senhas

--- api/projeto/src/main/resources/static/script/script.js
+++ api/projeto/src/main/resources/static/script/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 });
 const formulario = document.querySelector("form");
 const Inome = document.querySelector(".nome");
@@ -12,131 +12,155 @@ const Ibairro = document.querySelector(".bairro");
 const Iendereco = document.querySelector(".endereco");
 const Icontato = document.querySelector(".contato");
 const Isenha = document.querySelector(".senha");
+const Isenha1 = document.querySelector(".senha1");
 const Isexo = document.querySelector(".sexo");
 
 function cadastrar() {
-fetch("http://localhost:8080/usuarios", {
+  fetch("http://localhost:8080/usuarios", {
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json' 
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     },
     method: "POST",
     body: JSON.stringify({
-        nome: Inome.value,
-        cpf: Icpf.value,
-        datanascimento: Idatanascimento.value,
-        email: Iemail.value,
-        cep: Icep.value,
-        estado: Iestado.value,
-        cidade: Icidade.value,
-        bairro: Ibairro.value,
-        endereco: Iendereco.value,
-        contato: Icontato.value,
-        senha: Isenha.value,
-        sexo: Isexo.value
+      nome: Inome.value,
+      cpf: Icpf.value,
+      datanascimento: Idatanascimento.value,
+      email: Iemail.value,
+      cep: Icep.value,
+      estado: Iestado.value,
+      cidade: Icidade.value,
+      bairro: Ibairro.value,
+      endereco: Iendereco.value,
+      contato: Icontato.value,
+      senha: Isenha.value,
+      sexo: Isexo.value
 
     })
-})
-.then(function (res) { console.log(res) })
-.catch(function (res) { console.log(res) })
+  })
+    .then(function (res) { console.log(res) })
+    .catch(function (res) { console.log(res) })
 }
 
-function limpar () {
-Inome.value = "";
-Icpf.value = "";
-Idatanascimento.value = "";
-Iemail.value = "";
-Icep.value = "";
-Iestado.value = "";
-Icidade.value = "";
-Ibairro.value = "";
-Iendereco.value = "";
-Icontato.value = "";
-Isenha.value = "";
-Isexo.value = "";
+function limpar() {
+  Inome.value = "";
+  Icpf.value = "";
+  Idatanascimento.value = "";
+  Iemail.value = "";
+  Icep.value = "";
+  Iestado.value = "";
+  Icidade.value = "";
+  Ibairro.value = "";
+  Iendereco.value = "";
+  Icontato.value = "";
+  Isenha.value = "";
+  Isenha1.value = "";
+  Isexo.value = "";
 };
 
 formulario.addEventListener('submit', function (event) {
-event.preventDefault();
-console.log("Valor do sexo:", Isexo.value);
-cadastrar();
-limpar();
+  event.preventDefault();
+
 });
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    // Função para formatar a data
-    function formatarData(datanascimento) {
-      // Verifica se a data está no formato esperado
-      var regexData = /^\d{2}\/\d{2}\/\d{4}$/;
-      if (!regexData.test(datanascimento)) {
-        alert('Formato de data inválido');
-        return null;
-      }
-
-      // Divide a data em dia, mês e ano
-      var partes = datanascimento.split('/');
-      var dia = partes[0];
-      var mes = partes[1];
-      var ano = partes[2];
-
-      // Formata a data para o formato desejado (yyyy-mm-dd)
-      var dataFormatada = ano + '-' + mes + '-' + dia;
-
-      return dataFormatada;
+document.addEventListener('DOMContentLoaded', function () {
+  // Função para formatar a data
+  function formatarData(datanascimento) {
+    // Verifica se a data está no formato esperado
+    var regexData = /^\d{2}\/\d{2}\/\d{4}$/;
+    if (!regexData.test(datanascimento)) {
+      alert('Formato de data inválido');
+      return null;
     }
 
-    // Função para buscar informações do CEP
-    function buscarCep() {
-      var cep = document.querySelector('.cep').value.replace(/\D/g, ''); // Remove caracteres não numéricos
+    // Divide a data em dia, mês e ano
+    var partes = datanascimento.split('/');
+    var dia = partes[0];
+    var mes = partes[1];
+    var ano = partes[2];
 
-      if (cep.length !== 8) {
-        alert('CEP inválido');
-        return;
-      }
+    // Formata a data para o formato desejado (yyyy-mm-dd)
+    var dataFormatada = ano + '-' + mes + '-' + dia;
 
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://viacep.com.br/ws/' + cep + '/json/', true);
+    return dataFormatada;
+  }
 
-      xhr.onload = function () {
-        if (xhr.status === 200) {
-          var data = JSON.parse(xhr.responseText);
+  // Função para buscar informações do CEP
+  function buscarCep() {
+    var cep = document.querySelector('.cep').value.replace(/\D/g, ''); // Remove caracteres não numéricos
 
-          if (!data.erro) {
-            document.querySelector('.estado').value = data.uf;
-            document.querySelector('.cidade').value = data.localidade;
-            document.querySelector('.bairro').value = data.bairro;
-          } else {
-            alert('CEP não encontrado');
-          }
+    if (cep.length !== 8) {
+      alert('CEP inválido');
+      return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://viacep.com.br/ws/' + cep + '/json/', true);
+
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+
+        if (!data.erro) {
+          document.querySelector('.estado').value = data.uf;
+          document.querySelector('.cidade').value = data.localidade;
+          document.querySelector('.bairro').value = data.bairro;
         } else {
-          alert('Erro ao buscar CEP');
+          alert('CEP não encontrado');
         }
-      };
+      } else {
+        alert('Erro ao buscar CEP');
+      }
+    };
 
-      xhr.send();
-    }
+    xhr.send();
+  }
 
-    // Adiciona evento de blur para acionar a função ao sair do campo de CEP
-    document.querySelector('.cep').addEventListener('blur', function () {
-      buscarCep();
-    });
-
-
-
-    function recuperarSexo() {
-        console.log('Função recuperarSexo está sendo executada.');
-
-        const selectedValue = selectElement.value;
-
-        if (selectedValue) {
-            console.log('Valor do sexo:', selectedValue);
-            return selectedValue;
-        } else {
-            console.log('Valor do sexo: naoinformou');
-            return "naoinformou";
-        }
-    }
-
-
+  // Adiciona evento de blur para acionar a função ao sair do campo de CEP
+  document.querySelector('.cep').addEventListener('blur', function () {
+    buscarCep();
   });
+
+
+
+  function recuperarSexo() {
+    console.log('Função recuperarSexo está sendo executada.');
+
+    const selectedValue = selectElement.value;
+
+    if (selectedValue) {
+      console.log('Valor do sexo:', selectedValue);
+      return selectedValue;
+    } else {
+      console.log('Valor do sexo: naoinformou');
+      return "naoinformou";
+    }
+  }
+
+  Isenha.addEventListener('input', validarSenhas);
+  Isenha1.addEventListener('input', validarSenhas);
+
+  function validarSenhas() {
+    const senha = Isenha.value;
+    const senhaRepetida = Isenha1.value;
+
+    // Verifica se as senhas são iguais
+    return senha === senhaRepetida;
+  }
+
+  formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Se as senhas são iguais, então cadastra e limpa os campos
+    if (validarSenhas()) {
+      cadastrar();
+      limpar();
+    } else {
+      alert('Senhas são diferentes');
+    }
+  });
+
+
+
+});
